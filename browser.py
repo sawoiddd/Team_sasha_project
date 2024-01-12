7d69d3ead961dc6bf5f8f6a0ae9a6380dd826efb
Sashko123321 <154912849+Sashko123321@users.noreply.github.com>
2024-01-12 22:23:05 +0200

    Не знаю як зробити(код подивився в ютубі)

--- browser.py
+++ browser.py
@@ -1,1 +1,11 @@
-#hello
+import tkinterweb
+from tkinter import *
+import customtkinter
+
+root = customtkinter.CTk()
+
+frame= tkinterweb.HtmlFrame(root)
+frame.load_website('https://www.google.com/')
+frame.pack()
+
+root.mainloop()
